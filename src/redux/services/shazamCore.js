f5eab4d366bdbedb671ea9f2c117d52c53b51802
Asmit tyagi <ashmittyagigms@gmail.com>
2024-04-04 16:12:19 +0530

    add structure add store add discover add routing add other - First Commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+export const shazamCoreApi = createApi({
+    reducerPath:'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl:'https://shazam-core7.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '67911fa980msh544688ef29cacf6p1c8014jsna38dc06bb1a0');
+            headers.set('X-RapidAPI-Host', 'shazam-core7.p.rapidapi.com')
+
+            return headers
+        }
+    }),
+    endpoints:(builder) => ({
+        getTopCharts: builder.query({query: () => 'charts/get-top-songs-in_world_by_genre?genre=POP&limit=10'})
+    })
+})
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
