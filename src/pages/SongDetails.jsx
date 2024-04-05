5fc640df925e3605aa803080086d201c1f1bec30
Asmit tyagi <ashmittyagigms@gmail.com>
2024-04-05 14:00:40 +0530

    add RelatedSongs add relatedSongs api end point, complete SongDetails section

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,21 +2,34 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
-import { data } from "autoprefixer";
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
 	const dispatch = useDispatch();
-	const { songid } = useParams();
+	const { songid, id:artistId} = useParams();
 	const { activeSong, isPlaying } = useSelector((state) => state.player);
     const {data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({songid});
+    const {data, isFetching: isFetchingRelatedSongs, error} = useGetSongRelatedQuery({songid});
+
+    const handlePauseClick = () => {
+		dispatch(playPause(false));
+	};
+	const handlePlayClick = (song, i) => {
+		dispatch(setActiveSong({ song: song.song, data, i }));
+		dispatch(playPause(true));
+	};
 
 	// console.log(songData);
+    if(isFetchingRelatedSongs || isFetchingSongDetails) {
+        return <Loader title="Searching song details "/>
+    }
+    if(error) return <Error/>
+
 
 	return (
 		<div className="flex flex-col">
 			<DetailsHeader
-            artistId = ""
+            artistId = {artistId}
             songData= {songData}
         />
 
@@ -27,7 +40,12 @@ const SongDetails = () => {
             </div>
         </div>
         <RelatedSongs
-            
+            data={data}
+            isPlaying={isPlaying }
+            activeSong={activeSong}
+            handlePauseClick= {handlePauseClick}
+            handlePlayClick={handlePlayClick}
+
         />
 		</div>
 	);
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -15,10 +15,10 @@ export const shazamCoreApi = createApi({
     endpoints:(builder) => ({
         getTopCharts: builder.query({query: () => 'charts/get-top-songs-in_world_by_genre?genre=POP&limit=50'}),
         getSongDetails: builder.query({query: ({songid}) => `/songs/get_details?id=${songid}`}),
-        
+        getSongRelated: builder.query({query: ({songid}) => `/songs/list-recommendations?id=${songid}`})
 
     })
     
 })
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery } = shazamCoreApi;
